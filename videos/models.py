6c52231b40f7ba27279be285df8fb76263cab6f6
anuragdevon <msm19b021@iiitdm.ac.in>
2021-09-03 22:36:25 +0530

    add state options for Video

--- videos/models.py
+++ videos/models.py
@@ -1,18 +1,34 @@
 from django.db import models
-
-# Create your models here.
+from django.utils import timezone
 
 # Models for videos
 class Video(models.Model):
+  class VideoStateOptions(models.TextChoices): ###
+    PUBLISH = 'PU', 'Publish'
+    DRAFT = 'DR', 'Draft'
+
+  # Colums
   title = models.CharField(max_length=220)
   description = models.TextField(blank=True, null=True)
   slug = models.SlugField(blank=True, null=True) ###
   video_id = models.CharField(max_length=220)
   active = models.BooleanField(default=True)
+  state = models.CharField(max_length=2, choices=VideoStateOptions.choices) ###
+  publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
 
   @property
   def is_published(self):
     return self.active
+
+  # Specifying how to save      ###
+  def save(self, *args, **kwargs):
+    if self.state == self.VideoStateOptions.PUBLISH and self.publish_timestamp is None:
+      print("Save as timestamp as published")
+      self.published_timestamp = timezone.now()
+
+    elif self.state == self.VideoStateOptions.DRAFT:
+      self.publish_timestamp = None
+      super().save(*args, **kwargs) ###
 
 # Video Proxy
 class VideoAllProxy(Video): ###
