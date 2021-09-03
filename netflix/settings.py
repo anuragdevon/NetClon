8b9002d1236ea0309b90dbad81567f031979f3e2
anuragdevon <msm19b021@iiitdm.ac.in>
2021-09-03 20:37:00 +0530

    minor fixes

--- netflix/settings.py
+++ netflix/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netflix',
-        'USER': 'anurag',
+        'USER': 'postgres',
     }
 }
 
--- videos/models.py
+++ videos/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Video(mdoels.Model):
-  title = models.CharField()
-  description = models.TextField()
+class Video(models.Model):
+  title = models.CharField(max_length=220)
+  description = models.TextField(blank=True, null=True)
   slug = models.SlugField(blank=True, null=True)
-  video-id = models.CharField()
+  video_id = models.CharField(max_length=220)
