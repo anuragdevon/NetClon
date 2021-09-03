8b02613527f9f1e6ee99af026e2152631bd70dfe
anuragdevon <msm19b021@iiitdm.ac.in>
2021-09-03 23:18:08 +0530

    add slugs and timestamps

--- videos/admin.py
+++ videos/admin.py
@@ -1,32 +1,32 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# from .models import (
-#         Video,
-#         VideoAllProxy,
-#         VideoPublishedProxy
-# )
+from .models import (
+        Video,
+        VideoAllProxy,
+        VideoPublishedProxy
+)
 
-# # Admin register for all videos proxy
-# class VideoAllAdmin(admin.ModelAdmin):        ###
-#         list_display = ['title', 'id', 'state', 'video_id', 'is_published']
-#         search_fields = ['titile']
-#         list_filter = ['state', 'active']
-#         readonly_fields = ['id', 'is_published', 'publish_timestamp']    ###
+# Admin register for all videos proxy
+class VideoAllAdmin(admin.ModelAdmin):        ###
+        list_display = ['title', 'id', 'state', 'video_id', 'is_published']
+        search_fields = ['titile']
+        list_filter = ['state', 'active']
+        readonly_fields = ['id', 'is_published', 'publish_timestamp']    ###
 
-#         class Meta:
-#                 model = VideoAllProxy
+        class Meta:
+                model = VideoAllProxy
 
-# admin.site.register(VideoAllProxy, VideoAllAdmin)
+admin.site.register(VideoAllProxy, VideoAllAdmin)
 
-# # Admin register for all published video proxy
-# class VideoPublishedProxyAdmin(admin.ModelAdmin):
-#         list_display = ['title', 'video_id']
-#         search_fields = ['title']
-#         class Meta:
-#                 model = VideoPublishedProxy
+# Admin register for all published video proxy
+class VideoPublishedProxyAdmin(admin.ModelAdmin):
+        list_display = ['title', 'video_id']
+        search_fields = ['title']
+        class Meta:
+                model = VideoPublishedProxy
 
-#         def get_queryset(self, request):        ###
-#                 return VideoProxy.objects.filter(active=True)
+        def get_queryset(self, request):        ###
+                return VideoProxy.objects.filter(active=True)
 
-# admin.site.register(VideoPublishedProxy, VideoPublishedProxyAdmin)
+admin.site.register(VideoPublishedProxy, VideoPublishedProxyAdmin)
 
--- videos/models.py
+++ videos/models.py
@@ -1,34 +1,40 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
 # Models for videos
 class Video(models.Model):
-  class VideoStateOptions(models.TextChoices): ###
-    PUBLISH = 'PU', 'Publish'
-    DRAFT = 'DR', 'Draft'
+    class VideoStateOptions(models.TextChoices):    ###
+      PUBLISH = 'PU', 'Publish'
+      DRAFT = 'DR', 'Draft'
 
+# system break after commit - "add state options"    ###
   # Colums
-  title = models.CharField(max_length=220)
-  description = models.TextField(blank=True, null=True)
-  slug = models.SlugField(blank=True, null=True) ###
-  video_id = models.CharField(max_length=220)
-  active = models.BooleanField(default=True)
-  state = models.CharField(max_length=2, choices=VideoStateOptions.choices) ###
-  publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
+    title = models.CharField(max_length=220)
+    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(blank=True, null=True) ###
+    video_id = models.CharField(max_length=220, unique=True)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    state = models.CharField(max_length=2, choices=VideoStateOptions.choices) ###
+    publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
 
-  @property
-  def is_published(self):
-    return self.active
+    @property
+    def is_published(self):
+        return self.active
 
   # Specifying how to save      ###
-  def save(self, *args, **kwargs):
-    if self.state == self.VideoStateOptions.PUBLISH and self.publish_timestamp is None:
-      print("Save as timestamp as published")
-      self.published_timestamp = timezone.now()
+    def save(self, *args, **kwargs):
+        if self.state == self.VideoStateOptions.PUBLISH and self.publish_timestamp is None:
+            print("Save as timestamp as published")
+            self.published_timestamp = timezone.now()
 
-    elif self.state == self.VideoStateOptions.DRAFT:
-      self.publish_timestamp = None
-      super().save(*args, **kwargs) ###
+        elif self.state == self.VideoStateOptions.DRAFT:
+            self.publish_timestamp = None
+            if self.slug == None:
+                self.slug = slugify(self.title)
+            super().save(*args, **kwargs) ###
 
 # Video Proxy
 class VideoAllProxy(Video): ###
