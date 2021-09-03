8e9afa7852e8819133934f6c81c6255825dfc8d0
anuragdevon <msm19b021@iiitdm.ac.in>
2021-09-03 20:41:38 +0530

    add videos proxy

--- videos/models.py
+++ videos/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# Models for videos
 class Video(models.Model):
   title = models.CharField(max_length=220)
   description = models.TextField(blank=True, null=True)
-  slug = models.SlugField(blank=True, null=True)
+  slug = models.SlugField(blank=True, null=True) ###
   video_id = models.CharField(max_length=220)
+
+# Video Proxy
+class VideoProxy(Video): ###
+  class Meta:
+    proxy = True
+    verbose_name = 'Published Video'
+    verbose_name_plural = 'Published Videos'
